5d24fed7a7ae72a3554ce67916c2acf9446e3634
Zappy Code <madeupbypeople@gmail.com>
2018-06-17 14:54:08 +0900

    Initial Commit

---
+++ Collector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collector
+//
+//  Created by zappycode on 6/17/18.
+//  Copyright © 2018 Nick Walter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
